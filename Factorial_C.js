60b94fa038e9e2210cfbae72edef19d86c61a90f
Pasindu Sahan <pasindusahan2525@gmail.com>
2021-09-11 22:59:50 +0530

    Add chash clear meata TAG

--- Factorial_C.js
+++ Factorial_C.js
@@ -1,14 +1,20 @@
 function calculate() {
     var n = document.getElementById("inp1").value;
-    var i = n;
-    var fa = 1;
-    var result = "";
-    while (i > 1) {
-        fa = fa * i;
-        i--;
-        result += "*" + i;
-    }
+    if(n != ""){
 
-    document.getElementById("oup").innerHTML = "Factoriol Is : " + fa;
-    document.getElementById("pros").innerHTML = "[ " + n + result + " ]";
+        var i = n;
+        var fa = 1;
+        var result = "";
+        while (i > 1) {
+            fa = fa * i;
+            i--;
+            result += "*" + i;
+        }
+        
+        document.getElementById("oup").innerHTML = "Factoriol Is : " + fa;
+        document.getElementById("pros").innerHTML = "[ " + n + result + " ]";
+    }else{
+        document.getElementById("oup").innerHTML = "";
+        document.getElementById("pros").innerHTML = "Enter Number...!";
+    }
 }
