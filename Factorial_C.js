06dd9abf3a78d5f2bb68d956b1a690c1e9ab1394
Pasindu Sahan <pasindusahan2525@gmail.com>
2021-09-11 22:05:30 +0530

    First Commit...!

---
+++ Factorial_C.js
@@ -0,0 +1,14 @@
+function calculate() {
+    var n = document.getElementById("inp1").value;
+    var i = n;
+    var fa = 1;
+    var result = "";
+    while (i > 1) {
+        fa = fa * i;
+        i--;
+        result += "*" + i;
+    }
+
+    document.getElementById("oup").innerHTML = "Factoriol Is : " + fa;
+    document.getElementById("pros").innerHTML = "[ " + n + result + " ]";
+}
